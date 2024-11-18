e741e00078373b30c5d5d8f190d510a9d05bc14d
m-Mateja <>
2024-11-17 22:09:30 -0500

    websockets initialized

--- liquid-duck-frontend/src/app/services/data.service.ts
+++ liquid-duck-frontend/src/app/services/data.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../environment";
-
 @Injectable({
   providedIn: 'root'
 })
